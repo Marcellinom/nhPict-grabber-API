a7fd44edb72dc0c5f058de92b2f0b0a45611c40a
Marslel <marcellino.janitra@yahoo.com>
2021-01-30 23:06:11 +0700

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const nhentai = require('nhentai-js')
 // const api = new API();
 const express = require('express')
 const app = express()
-const port = 3000
+let port = process.env.PORT || 3000 
 
 app.listen(port, () => { console.log(`app listening at http://localhost:${port}`)
     app.get('/:code', (req, res) => {
