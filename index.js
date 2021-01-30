59587cbcf2ffe48b96693047480323cb0b65f419
Marslel <marcellino.janitra@yahoo.com>
2021-01-30 22:23:33 +0700

    replacing to lighter version

---
+++ index.js
@@ -0,0 +1,20 @@
+const nhentai = require('nhentai-js')
+// const api = new API();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, () => { console.log(`app listening at http://localhost:${port}`)
+    app.get('/:code', (req, res) => {
+        async function getDoujin(id){
+            try{ // try/catch is the equivalent of Promise.catch() in async/await
+                const val = await nhentai.getDoujin(id)
+               res.send(val)
+                // console.log(val);
+            }catch(err){
+                console.error(err);
+            }
+        }getDoujin(req.params.code)
+    })
+})
+    
