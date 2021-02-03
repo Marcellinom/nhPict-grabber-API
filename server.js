707017ebdfaec8dbe9c5835ea8fc79b3258afefe
Marslel <marcellino.janitra@yahoo.com>
2021-02-03 08:26:51 +0700

    fix port yang hilang

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
 // const api = new API();
 const nhentai = require('nhentai-js');
 const express = require('express');
-const fs = require('fs');
-const fetch = require('node-fetch');
 const app = express();
-
+let port = process.env.PORT || 3000 
 
 app.listen(port, () => { console.log(`app listening at http://localhost:${port}`)
-app.get('/popular/', (req, res) => {
-    async function homePage(){
-        try{ // try/catch is the equivalent of Promise.catch() in async/await
-            let val = await nhentai.getHomepage(1)
-            res.send(val)
-        }catch(err){
-            res.send(err)
-        }
-    }homePage()
-})
-app.get('/', (req, res) => {
-    res.send('to use: com/(nhentai code)')
-})
-app.get('/:code', (req, res) => {
-    async function getDoujin(id){
-        try{ // try/catch is the equivalent of Promise.catch() in async/await
-            let val = await nhentai.getDoujin(id)
-            res.send(val)
-        }catch(err){
-            res.send(err)
-        }
-    }getDoujin(req.params.code)
-})
+    app.get('/popular/', (req, res) => {
+        async function homePage(){
+            try{ // try/catch is the equivalent of Promise.catch() in async/await
+                let val = await nhentai.getHomepage(1)
+                res.send(val)
+            }catch(err){
+                res.send(err)
+            }
+        }homePage()
+    })
+    app.get('/', (req, res) => {
+        res.send('to use: com/(nhentai code)')
+    })
+    app.get('/:code', (req, res) => {
+        async function getDoujin(id){
+            try{ // try/catch is the equivalent of Promise.catch() in async/await
+                let val = await nhentai.getDoujin(id)
+                res.send(val)
+            }catch(err){
+                res.send(err)
+            }
+        }getDoujin(req.params.code)
+    })
 })
 
